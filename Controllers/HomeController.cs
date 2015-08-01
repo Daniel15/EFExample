a3dec08b0f6d02bdadd57368bdce0f93ad09d891
Daniel Lo Nigro <daniel@dan.cx>
2015-08-01 16:31:43 -0700

    Add MVC and configuration

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Daniel15.EFTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+	        return Content("Hello World");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,26 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Runtime;
 
 namespace Daniel15.EFTest
 {
-    public class Startup
-    {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+		{
+			// Setup configuration sources.
+			var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+				.AddJsonFile("config.json")
+				.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+		public IConfiguration Configuration { get; set; }
+
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+			services.AddSingleton<IConfiguration>(_ => Configuration);
+		}
+
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}"
+				);
+			});
+		}
+	}
 }
